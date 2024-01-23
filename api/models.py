ae32f5faedcfbe9374529792f039fb23effae1ec
David Garay <davidgaraycr@gmail.com>
2024-01-22 19:33:50 -0500

    Correccion de modelos y campos

--- api/models.py
+++ api/models.py
@@ -4,13 +4,14 @@ from django.db import models
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length = 100)
+    apellidos = models.CharField(max_length = 100)
     Email = models.CharField(max_length = 150)
     password =  models.CharField(max_length = 30)
 
 class Archivo(models.Model):
-    archivo = models.FileField(upload_to='api/')
-    fecha = models.DateField(max_length=10,null=False,blank=False)
-    propietario = models.CharField(max_length = 100)
+    archivo = models.FileField(upload_to='files/')
+    fecha = models.DateField(auto_now_add=True)
+    propietario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'{self.propietario}'
